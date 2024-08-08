863d589b10f0e825bbebe0b295898bcdac1ed42b
Eduarda Escócio <157810281+EduardaEscocio@users.noreply.github.com>
2024-08-08 20:26:38 -0300

    Funções de mudar no tabuleiro corrigidas

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import random
 sorteio = random.randint(1,2)
 humano = 'X'
-computador = 'O'
+computador = '\033[0;31mO\033[m'
+
 tabuleiro = [[0,0,0], [0,0,0], [0,0,0]]
 
 def imprimir_tabuleiro(tabuleiro):
@@ -14,30 +15,49 @@ def imprimir_tabuleiro(tabuleiro):
 def checar_casa_human(tabuleiro):
     while True:
         linha_human = int(input('Escolha a linha para jogar: '))
+        
         coluna_human = int(input('Escolha a coluna para jogar: '))
         if tabuleiro[linha_human][coluna_human] == 0:
-            tabuleiro.insert([linha_human][coluna_human], humano)
+            tabuleiro[linha_human][coluna_human] = 'X'
 
-        elif tabuleiro[linha_human][coluna_human] != 0:
+        else:
             print(f'A casa de linha: {linha_human} e coluna: {coluna_human} está ocupada, escolha outra')
-
+        break
 
 def checar_casa_comp(tabuleiro):
     while True:
         linha_comp = random.randint(0,2)
         coluna_comp = random.randint(0,2)
-        if linha_comp not in range(3) or coluna_comp not in range(3):
-            return linha_comp, coluna_comp
+        if tabuleiro[linha_comp][coluna_comp] == 0:
+            tabuleiro[linha_comp][coluna_comp] = computador
+        break
       
 
 while True:
     imprimir_tabuleiro(tabuleiro=tabuleiro)
+    print(' ')
+    print('=*' * 40)
+    print(' ')
     if sorteio == 1:
         checar_casa_comp(tabuleiro=tabuleiro)
+        print('=*' * 40)
+        print(' ')
+
+        imprimir_tabuleiro(tabuleiro=tabuleiro)
+        print('=*' * 40)
+        print(' ')
+
         checar_casa_human(tabuleiro=tabuleiro)
 
     elif sorteio == 2:
         checar_casa_human(tabuleiro=tabuleiro)
+        print('=*' * 40)
+        print(' ')
+
+        imprimir_tabuleiro(tabuleiro=tabuleiro)
+        print('=*' * 40)
+        print(' ')
+
         checar_casa_comp(tabuleiro=tabuleiro)
 
 # def escolher_jog(sorteio):
