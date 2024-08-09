6778e60362d09c0db95f7f9328614ef0c2c5daec
Eduarda Escócio <157810281+EduardaEscocio@users.noreply.github.com>
2024-08-09 12:52:35 -0300

    Funções de verificar ganhador na vertical e horizontal concluidas

--- index.py
+++ index.py
@@ -4,7 +4,8 @@ humano = 'X'
 computador = '\033[0;31mO\033[m'
 
 tabuleiro = [[0,0,0], [0,0,0], [0,0,0]]
-
+def menu():
+    jogador = input('Insira seu nome: ')
 def imprimir_tabuleiro(tabuleiro):
     
     for i in range(3):
@@ -22,6 +23,7 @@ def checar_casa_human(tabuleiro):
 
         else:
             print(f'A casa de linha: {linha_human} e coluna: {coluna_human} está ocupada, escolha outra')
+            checar_casa_human(tabuleiro=tabuleiro)
         break
 
 def checar_casa_comp(tabuleiro):
@@ -30,16 +32,34 @@ def checar_casa_comp(tabuleiro):
         coluna_comp = random.randint(0,2)
         if tabuleiro[linha_comp][coluna_comp] == 0:
             tabuleiro[linha_comp][coluna_comp] = computador
+        else: 
+            print('A casa escolhida pelo computador já está ocupada, ele irá jogar novamente!.')
+            checar_casa_comp(tabuleiro=tabuleiro)
         break
       
-
+def verificar_vertical():
+    for i in range(2):
+        if tabuleiro[0][i] == 'X' and tabuleiro[1][i] == 'X' and tabuleiro[2][i] == 'X':
+            print(f'\033[0;31mO jogador {jogador} ganhou \033[m')
+        if tabuleiro[0][i] == computador and tabuleiro[1][i] == computador and tabuleiro[2][i] == computador:
+            print('O computador ganhou')
+            break
+def verificar_horizontal():
+    for i in range(2):
+        if tabuleiro[i][0] == 'X' and tabuleiro[i][1] == 'X' and tabuleiro[i][2] == 'X':
+            print('O jogador X ganhou')
+        if tabuleiro[i][0] == computador and tabuleiro[i][1] == computador and tabuleiro[i][2] == computador:
+            print('O computador ganhou')
+            break
 while True:
+    menu()
     imprimir_tabuleiro(tabuleiro=tabuleiro)
     print(' ')
     print('=*' * 40)
     print(' ')
     if sorteio == 1:
         checar_casa_comp(tabuleiro=tabuleiro)
+        verificar_vertical()
         print('=*' * 40)
         print(' ')
 
@@ -48,9 +68,10 @@ while True:
         print(' ')
 
         checar_casa_human(tabuleiro=tabuleiro)
-
+        verificar_vertical()
     elif sorteio == 2:
         checar_casa_human(tabuleiro=tabuleiro)
+        verificar_vertical()
         print('=*' * 40)
         print(' ')
 
@@ -59,7 +80,7 @@ while True:
         print(' ')
 
         checar_casa_comp(tabuleiro=tabuleiro)
-
+        verificar_vertical()
 # def escolher_jog(sorteio):
 #     if sorteio == 1:
 #         checar_casa_comp()
