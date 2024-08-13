313d97bf2f1996dcaf64d5990e1974df967f301e
Eduarda Escócio <157810281+EduardaEscocio@users.noreply.github.com>
2024-08-13 08:17:46 -0300

    Código comentado

--- index.py
+++ index.py
@@ -8,6 +8,72 @@ placar_X = 0
 placar_O = 0
 count = 0
 
+#IMPRIMIR O TABULEIRO
+def imprimir_tabuleiro(tabuleiro):
+    print('\033[0;94m  1   2   3\033[m')
+    print()
+    for i in range(3):
+        print(f'{f'\033[0;94m{i+1}\033[m'} '  f'{tabuleiro[i][0]} | {tabuleiro[i][1]} | {tabuleiro[i][2]} ')
+        if i < 2:
+            print(' ---+---+---')
+
+#VERIFICAR SE A CASA JOGADA PELO HUMANO ESTÁ DISPONÍVEL, SE SIM, JOGAR
+def checar_casa_human(tabuleiro):
+    linha_human = None
+    coluna_human = None
+
+    while True:
+        linha_human = validarinputlinha()
+        coluna_human = validarinputcoluna()
+   
+        if not checarvalidh(linha_human, coluna_human):
+            continue
+        else:
+            tabuleiro[linha_human][coluna_human] = 'X'
+            break
+
+#CHECAR SE A COLUNA ESCOLHIDA PELO HUMANO ESTÁ VALIDA NO TABULEIRO
+def validarinputcoluna():
+    while True:
+        coluna_human = int(input('Escolha a coluna para jogar: ') ) - 1
+        if coluna_human > 2 or coluna_human < 0:
+            print('Coluna invalida')
+        else:
+            break
+    return coluna_human
+
+#CHECAR SE A LINHA ESCOLHIDA PELO HUMANO ESTÁ VÁLIDA NO TABULEIRO
+def validarinputlinha():
+    while True:
+        linha_human = int(input('Escolha a linha para jogar: ') ) - 1 
+        if linha_human > 2 or linha_human < 0:
+            print('Linha inválida')
+        else: 
+            break
+    return linha_human
+
+#CHECAR SE A CASA JOGADA PELO HUMANO ESTÁ DISPONÍVEL
+def checarvalidh(linha_human, coluna_human):
+    if tabuleiro[linha_human][coluna_human] == ' ':
+        return True
+    else: 
+        print('Casa inválida')
+        return False
+
+#CHECAR A LINHA E A COLUNA QUE O COMPUTADOR ESCOLHEU E FAZER A SUA JOGADA
+def checar_casa_computador(tabuleiro):
+    while True:
+        linha_comp = random.randint(0,2)
+        coluna_comp = random.randint(0,2)
+        if tabuleiro[linha_comp][coluna_comp] == ' ':
+            tabuleiro[linha_comp][coluna_comp] = computador
+            print(f'O computador jogou na casa de linha {linha_comp+1} e coluna {coluna_comp+1}')
+        else: 
+            print(f'A casa de linha {linha_comp} e coluna {coluna_comp} escolhida pelo computador já está ocupada, ele irá jogar novamente!.')
+            checar_casa_computador(tabuleiro=tabuleiro)
+        break
+
+#FUNÇÕES PARA VERIFICAR UM GANHADOR 
 def verificar_vertical():
     vencedor = ' '
     for i in range(2):
@@ -54,81 +120,6 @@ def verificar_diagonal():
             print(f'\033[0;32mO computador ganhou 1 \033[m')
             return vencedor
     
-def imprimir_tabuleiro(tabuleiro):
-    print('\033[0;94m  1   2   3\033[m')
-    print()
-    for i in range(3):
-        print(f'{f'\033[0;94m{i+1}\033[m'} '  f'{tabuleiro[i][0]} | {tabuleiro[i][1]} | {tabuleiro[i][2]} ')
-        if i < 2:
-            print(' ---+---+---')
-
-def checar_casa_human(tabuleiro):
-    linha_human = None
-    coluna_human = None
-
-    while True:
-        linha_human = validarinputlinha()
-        coluna_human = validarinputcoluna()
-   
-        if not checarvalidh(linha_human, coluna_human):
-            continue
-        else:
-            tabuleiro[linha_human][coluna_human] = 'X'
-            break
-
-def validarinputcoluna():
-    while True:
-        coluna_human = int(input('Escolha a coluna para jogar: ') ) - 1
-        if coluna_human > 2 or coluna_human == -1:
-            print('Coluna invalida')
-        else:
-            break
-    return coluna_human
-
-def validarinputlinha():
-    while True:
-        linha_human = int(input('Escolha a linha para jogar: ') ) - 1 
-        if linha_human > 2 or linha_human == -1:
-            print('Linha inválida')
-        else: 
-            break
-    return linha_human
-        
-def checarlinhah():
-        linha_human = int(input('Escolha a linha para jogar: ')) -1 
-        if linha_human > 2 or linha_human == -1:
-             print('Linha inválida')
-             checarlinhah()
-        else:
-            return linha_human
-
-def checarcolunah():
-        coluna_human = int(input('Escolha a coluna para jogar: ')) - 1
-        if coluna_human > 2 or coluna_human == -1:
-            print('Coluna inválida')
-            checarcolunah()
-        else:
-            return coluna_human
-
-def checarvalidh(linha_human, coluna_human):
-    if tabuleiro[linha_human][coluna_human] == ' ':
-        return True
-    else: 
-        print('Casa inválida')
-        return False
-
-
-def checar_casa_comp(tabuleiro):
-    while True:
-        linha_comp = random.randint(0,2)
-        coluna_comp = random.randint(0,2)
-        if tabuleiro[linha_comp][coluna_comp] == ' ':
-            tabuleiro[linha_comp][coluna_comp] = computador
-            print(f'O computador jogou na casa de linha {linha_comp+1} e coluna {coluna_comp+1}')
-        else: 
-            print(f'A casa de linha {linha_comp} e coluna {coluna_comp} escolhida pelo computador já está ocupada, ele irá jogar novamente!.')
-            checar_casa_comp(tabuleiro=tabuleiro)
-        break
 
 # AVISAR AO PLAYER QUEM INICIOU A PARTIDA
 if sorteio == 1:
@@ -148,7 +139,7 @@ def main():
         print(' ')
         if sorteio == 1:
             
-            checar_casa_comp(tabuleiro=tabuleiro)
+            checar_casa_computador(tabuleiro=tabuleiro)
             jogadas += 1
             if verificar_horizontal() == computador or verificar_vertical() == computador or verificar_diagonal() == computador:
                 placar_O += 1
@@ -189,7 +180,7 @@ def main():
             if jogadas > 8:
                 print('Deu velha')
                 break
-            checar_casa_comp(tabuleiro=tabuleiro)
+            checar_casa_computador(tabuleiro=tabuleiro)
             jogadas += 1
             if verificar_horizontal() == computador or verificar_vertical() == computador or verificar_diagonal() == computador:
                 placar_O += 1
